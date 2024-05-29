1f606724afc74d7cd0d58d3d26b66271a7a755d8
prasanthi <vemulalakshmiprashanthi@gmail.com>
2024-05-29 17:26:49 +0530

    first commit

---
+++ src/com/test/Test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Test {
+    public static void main(String[] args) {
+        int num = 10;
+        while (num <= 50) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
+
+
